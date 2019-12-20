5163014dd1c0512566e9d1aa19811aff74e9beb4
tauan-longaretti <tauanlongaretti@gmail.com>
2019-12-20 00:51:55 -0500

    added additional form components to SignIn and SignUp

--- src/Components/SignInForm.js
+++ src/Components/SignInForm.js
@@ -5,7 +5,7 @@ const FormStyling = styled.form`
     display: block;
     align-content: center;
     padding-bottom: 10px;
-    margin: 3%;
+    margin: 8%;
 `;
 
 const InputStyling = styled.input`
@@ -13,9 +13,25 @@ const InputStyling = styled.input`
     margin: 1% 12%;
 `;
 
+const ButtonStyling = styled.button`
+    border-radius: 6px;
+    border: none;
+    width: 15%;
+    height: 30px;
+    margin: 3% 25%;
+    color: white;
+    background-color: #19BD60;
+`;
+
+const AnchorStyling = styled.a`
+  text-decoration: none;
+  color: #1FA9AB;
+`;
+
 export default function SignIn() {
     return (
         <FormStyling>
+           <h1>Welcome Back!</h1>
            <InputStyling placeholder="Email"
            id="email"
            type="text"
@@ -26,6 +42,13 @@ export default function SignIn() {
            type="password"
            name="password"
            />
+           <ButtonStyling type="submit">
+               Login
+           </ButtonStyling>
+           <div>
+             <span>New Here?</span>
+             <AnchorStyling href=""> Create an Account</AnchorStyling>
+           </div>
        </FormStyling> 
     )
 }
