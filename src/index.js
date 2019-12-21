ae857ba6b7330de1bea02e42baf1cbb0261be577
Jason Barr <jason@jasonsbarr.com>
2019-12-21 12:06:51 -0600

    Create test component to demonstrate usage for team

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
+import { useHttpRequest } from "@jasonsbarr/custom-hooks";
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+const HookTest = () => {
+  const [render, handleRequest] = useHttpRequest({
+    initialState: "This is the initial state",
+    pendingState: "Loading...",
+    config: {
+      baseURL: "https://jsonplaceholder.typicode.com",
+    },
+  });
+
+  return render({
+    // do this to fire the request in response to an event
+    initial: () => (
+      <button
+        onClick={() =>
+          handleRequest("/posts?_limit=10", {
+            config: { method: "get" },
+          })
+        }
+      >
+        Request
+      </button>
+    ),
+    pending: () => "Loading...",
+    error: err => JSON.stringify(err),
+    success: data => JSON.stringify(data),
+  });
+};
+
+ReactDOM.render(
+  <Router>
+    <HookTest />
+  </Router>,
+  document.getElementById("root"),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
