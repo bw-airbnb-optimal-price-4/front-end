ff24cab51c38c9415e79a77f22ff330331e0b8c4
tauan-longaretti <tauanlongaretti@gmail.com>
2019-12-20 23:59:55 -0500

    basic *onboarding* structure created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./App.css";
-import Navigation from "./Components/Header";
 import SignInForm from "./Components/SignInForm";
 import SignUpForm from "./Components/SignUpForm";
+import Onboarding from "./Components/Onboarding/Onboarding";
+import OnboardingTwo from "./Components/Onboarding/OnboardingTwo";
 import Footer from "./Components/Footer";
 import { Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <Route exact path="/" component={SignInForm}/>
+      <Route exact path="/signin" component={SignInForm}/>
       <Route exact path="/signup" component={SignUpForm}/>
+      <Route exact path="/onboarding" component={Onboarding}/>
+      <Route exact path="/onboardingtwo" component={OnboardingTwo}/>
       <Footer/>
     </div>    
   );
