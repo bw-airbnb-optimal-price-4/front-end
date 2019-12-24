a6c22daecb2629325c1e99deaaafd410d20e93dc
Jason Barr <jason@jasonsbarr.com>
2019-12-24 03:10:44 -0500

    Set up auth context with initial state

--- src/contexts/auth-context.js
+++ src/contexts/auth-context.js
@@ -4,7 +4,36 @@ import {
   useLocalStorage,
   useHttpRequest,
 } from "@jasonsbarr/custom-hooks";
+import { makeReducer } from "@jasonsbarr/reducer-utils";
 
 const AuthContext = createContext();
 
-const AuthProvider = () => {};
+const login = () => {};
+const register = () => {};
+const logout = () => {};
+
+const initialState = {
+  data: { user: null },
+  login,
+  register,
+  logout,
+};
+
+const reducer = makeReducer(
+  {
+    SET_AUTH: (state, payload) => ({ ...state, data: payload }),
+  },
+  initialState,
+);
+
+const AuthProvider = ({ children }) => {
+  return (
+    <StateProvider reducer={reducer} stateContext={AuthContext}>
+      {children}
+    </StateProvider>
+  );
+};
+
+const useAuth = useStateValue(AuthContext);
+
+export { AuthProvider, useAuth };
