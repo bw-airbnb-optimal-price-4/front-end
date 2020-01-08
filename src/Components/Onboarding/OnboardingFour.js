fd2bcf5964cc22ac6b82557e8c53171d5aaac8eb
tauan-longaretti <tauanlongaretti@gmail.com>
2020-01-07 23:42:32 -0500

    made adjustments on styling

--- src/Components/Onboarding/OnboardingFour.js
+++ src/Components/Onboarding/OnboardingFour.js
@@ -7,12 +7,30 @@ const DivStyling = styled.div`
   text-align: center;
 `;
 
+const DescriptionStyling = styled.div`
+  display: block;
+  line-height: 4;
+  margin: 3% 35% 0%;
+  justify-content: center;
+`;
+
+const PicUploadStyling = styled.div`
+  display: block;
+  line-height: 4;
+  margin: 0% 40% 3%;
+  justify-content: center;
+`;
+
+const AmenitiesStyling = styled.div`
+  margin-top: 2%;
+`;
+
 const ButtonStyling = styled.button`
   border-radius: 6px;
   border: none;
   width: 16%;
   height: 30px;
-  margin: 5% 42% 5% 42%;
+  margin: 5% 42% 0% 42%;
   color: white;
   background-color: #19bd60;
 `;
@@ -23,49 +41,53 @@ export default function OnboardingFour() {
       <Header />
       <DivStyling>
         <h2>Please provide additional property information</h2>
-        <h3>Step 3 of 3</h3>
+        <h3>Step 3 of 3</h3>      
         <form>
-          <div>
-            <textarea rows="2" cols="50" name="description" placeholder="Enter property description here..."></textarea>
-          </div>
-          <div>
+          <DescriptionStyling>  
+            <label>Provide a short description of your property:
+            <textarea rows="3" cols="50" name="description" placeholder="Enter property description here..."></textarea>
+            </label>
+          </DescriptionStyling>
+          <PicUploadStyling>
             <label>Upload a picture to show off your property:
             <input type="file"
             id="avatar" name="avatar"
             accept="image/png, image/jpeg"/>
             </label>
-          </div>  
+          </PicUploadStyling>  
           <p>Please check all of the following amenities your property offers:</p>
-          <label>TV 
-            <input type="checkbox" name="tv"/>
-          </label>
-          <label>Internet 
-            <input type="checkbox" name="internet"/>
-          </label>
-          <label>WiFi 
-            <input type="checkbox" name="wifi"/>
-          </label>
-          <label>Kitchen 
-            <input type="checkbox" name="kitchen"/>
-          </label>
-          <label>Pet-Friendly 
-            <input type="checkbox" name="pets"/>
-          </label>
-          <label>Breakfast 
-            <input type="checkbox" name="breakfast"/>
-          </label>
-          <label>Free Parking 
-            <input type="checkbox" name="free-parking"/>
-          </label>
-          <label>Hot Tub 
-            <input type="checkbox" name="hot-tub"/>
-          </label>
-          <label>Air Condintioning 
-            <input type="checkbox" name="air-conditioning"/>
-          </label>
-          <label>Private Entrance 
-            <input type="checkbox" name="private-entrance"/>
-          </label>
+          <AmenitiesStyling>
+            <label>TV 
+              <input type="checkbox" name="tv"/>
+            </label>
+            <label>Internet 
+              <input type="checkbox" name="internet"/>
+            </label>
+            <label>WiFi 
+              <input type="checkbox" name="wifi"/>
+            </label>
+            <label>Kitchen 
+              <input type="checkbox" name="kitchen"/>
+            </label>
+            <label>Pet-Friendly 
+              <input type="checkbox" name="pets"/>
+            </label>
+            <label>Breakfast 
+              <input type="checkbox" name="breakfast"/>
+            </label>
+            <label>Free Parking 
+              <input type="checkbox" name="free-parking"/>
+            </label>
+            <label>Hot Tub 
+              <input type="checkbox" name="hot-tub"/>
+            </label>
+            <label>Air Condintioning 
+              <input type="checkbox" name="air-conditioning"/>
+            </label>
+            <label>Private Entrance 
+              <input type="checkbox" name="private-entrance"/>
+            </label>
+          </AmenitiesStyling>
         </form>
         <ButtonStyling>Get your quote</ButtonStyling>
       </DivStyling>
